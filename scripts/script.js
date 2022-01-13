d5fbade0aabd1483d53e9991ef007159f7783675
Imran <hiimrann@gmail.com>
2022-01-13 23:33:56 +0800

    1. when a project is clicked, a modal appears in the middle of the screen containing all the relevant information about the project 2. restructured the code to be cleaner in index.html, and more specific in style.css 3. updated the ionicon script in index.html as the old one was deprecated 4. each project now contain screenshots in both desktop and mobile views 5. updated the z-index from large numbers (100, 200) to smaller numbers (1, 2) 6. updated the font-size to make texts appear larger 7. increased margin-bottom below the projects from 20px to 50px 8. updated the media queries to reflect above changes

---
+++ scripts/script.js
@@ -0,0 +1,36 @@
+"use strict!";
+
+let overlay = document.querySelector(".overlay");
+let project = document.querySelectorAll(".project");
+let modal = document.querySelectorAll(".modal");
+let btnCloseModal = document.querySelectorAll(".close-modal");
+
+for (let i = 0; i < modal.length; i++) {
+  // function for opening modals
+  let openModal = function () {
+    modal[i].classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  };
+
+  // function for closing modals
+  let closeModal = function () {
+    modal[i].classList.add("hidden");
+    overlay.classList.add("hidden");
+  };
+
+  // when a project is clicked
+  project[i].addEventListener("click", openModal);
+
+  // when the close button in a modal is clicked
+  btnCloseModal[i].addEventListener("click", closeModal);
+
+  // when the area outside a modal is clicked
+  overlay.addEventListener("click", closeModal);
+
+  // when the escape key is pressed
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "Escape") {
+      closeModal();
+    }
+  });
+}
